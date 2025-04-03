f54e7f16c6606a08f6270d377f8ae6117d9b2e6d
User <User@WIN-MJ6LE0TGPAD>
2025-04-03 09:05:36 +0300

    Добавьте файлы проекта.

---
+++ Book_masif/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_masif
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[][] book = new string[3][];
+
+            for (int i = 0; i < book.Length; i++)
+            {
+                Console.WriteLine("Введите имя книги: ");
+               string namebook = Console.ReadLine();
+
+                Console.WriteLine("Введите автора: ");
+                string nameavtor = Console.ReadLine();
+
+
+                book[i] = new string[] { namebook,nameavtor};
+            }
+            Console.WriteLine("\nВы ввели: ");
+            for (int i = 0;i < book.Length;i++)
+            {
+                
+                Console.WriteLine($"Книгу:{book[i][0]} - {book[i][1]} ");
+            }
+            
+        }
+    }
+}
